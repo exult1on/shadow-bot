0b67da34239be0c8d7cc1273b7ec832b5a424a23
exult1on <exult@gmx.com>
2024-04-06 18:02:30 +0200

    Guild check for extension manager

--- cogs/ext_mng.py
+++ cogs/ext_mng.py
@@ -1,3 +1,4 @@
+import settings
 from discord.ext import commands
 
 class exts(commands.Cog):
@@ -8,43 +9,45 @@ class exts(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def load(self, ctx, type: str, command: str):
-        if type == "cog":
-            type = "cogs"
-        elif type == "command":
-            type = "commands"
-        else:
-            await ctx.send(f"Unexpected type")
+        if settings.ServerID == ctx.guild.id:
+            if type == "cog":
+                type = "cogs"
+            elif type == "command":
+                type = "commands"
+            else:
+                await ctx.send(f"Unexpected type")
 
-        try:
-            await self.bot.load_extension(f"{type}.{command}")
-            await ctx.send(f"The {command} {type} has been loaded")
-            print(f"{command}.py loaded")
+            try:
+                await self.bot.load_extension(f"{type}.{command}")
+                await ctx.send(f"The {command} {type} has been loaded")
+                print(f"{command}.py loaded")
 
-        except commands.ExtensionNotFound:
-            await ctx.send(f"Command not found")
+            except commands.ExtensionNotFound:
+                await ctx.send(f"Command not found")
 
-        except commands.ExtensionAlreadyLoaded:
-            await self.bot.reload_extension(f"{type}.{command}")
-            await ctx.send(f"The {command} {type} already loaded, reloading")
-            print(f"{command}.py reloaded")
+            except commands.ExtensionAlreadyLoaded:
+                await self.bot.reload_extension(f"{type}.{command}")
+                await ctx.send(f"The {command} {type} already loaded, reloading")
+                print(f"{command}.py reloaded")
         
     @commands.command()
     @commands.is_owner()
     async def unload(self, ctx, command: str):
-        if type == "cog":
-            type = "cogs"
-        elif type == "command":
-            type = "commands"
-        else:
-            await ctx.send(f"Unexpected type")
+        if settings.ServerID == ctx.guild.id:
+            if type == "cog":
+                type = "cogs"
+            elif type == "command":
+                type = "commands"
+            else:
+                await ctx.send(f"Unexpected type")
 
-        try:
-            await self.bot.unload_extension(f"{type}.{command}")
-            await ctx.send(f"The {command} {type} has been unloaded")
-            print(f"{command}.py unloaded")
+            try:
+                await self.bot.unload_extension(f"{type}.{command}")
+                await ctx.send(f"The {command} {type} has been unloaded")
+                print(f"{command}.py unloaded")
 
-        except commands.ExtensionNotLoaded:
-            await ctx.send(f"The {command} {type} is not loaded")
+            except commands.ExtensionNotLoaded:
+                await ctx.send(f"The {command} {type} is not loaded")
         
 async def setup(bot):
     await bot.add_cog(exts(bot))
