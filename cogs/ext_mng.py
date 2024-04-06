b75cc753ec8ea5ba4a09dfde42e1fbe3add66121
exult1on <exult@gmx.com>
2024-04-06 22:45:21 +0200

    Change to the extension manager
    Changed the guild check from an if statement to a commands.check
    Added a prevention from softlocking by not allowing to unload the extension manager

--- cogs/ext_mng.py
+++ cogs/ext_mng.py
@@ -1,6 +1,9 @@
 import settings
 from discord.ext import commands
 
+def guildcheck(ctx):
+    return ctx.guild.id == settings.ServerID
+
 class exts(commands.Cog):
 
     def __init__(self, bot):
@@ -8,32 +11,35 @@ class exts(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
+    @commands.check(guildcheck)
     async def load(self, ctx, type: str, command: str):
-        if settings.ServerID == ctx.guild.id:
-            if type == "cog":
-                type = "cogs"
-            elif type == "command":
-                type = "commands"
-            else:
-                await ctx.send(f"Unexpected type")
+        if type == "cog":
+            type = "cogs"
+        elif type == "command":
+            type = "commands"
+        else:
+            await ctx.send(f"Unexpected type")
 
-            try:
-                await self.bot.load_extension(f"{type}.{command}")
-                await ctx.send(f"The {command} {type} has been loaded")
-                print(f"{command}.py loaded")
+        try:
+            await self.bot.load_extension(f"{type}.{command}")
+            await ctx.send(f"The {command} {type} has been loaded")
+            print(f"{command}.py loaded")
 
-            except commands.ExtensionNotFound:
-                await ctx.send(f"Command not found")
+        except commands.ExtensionNotFound:
+            await ctx.send(f"Command not found")
 
-            except commands.ExtensionAlreadyLoaded:
-                await self.bot.reload_extension(f"{type}.{command}")
-                await ctx.send(f"The {command} {type} already loaded, reloading")
-                print(f"{command}.py reloaded")
+        except commands.ExtensionAlreadyLoaded:
+            await self.bot.reload_extension(f"{type}.{command}")
+            await ctx.send(f"The {command} {type} already loaded, reloading")
+            print(f"{command}.py reloaded")
         
     @commands.command()
     @commands.is_owner()
+    @commands.check(guildcheck)
     async def unload(self, ctx, type: str, command: str):
-        if settings.ServerID == ctx.guild.id:
+        if command == "ext_mng":
+            await ctx.send(f"Can't unload the extension manager or the bot will be softlocked, try reloading")
+        else:
             if type == "cog":
                 type = "cogs"
             elif type == "command":
