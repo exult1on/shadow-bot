f8e70e32c5181cdc611b55752dd393932fd7b1f2
exult1on <exult@gmx.com>
2024-04-16 13:53:19 +0200

    Owner commands
    Added a command only accessible for the owner of the bot

---
+++ cogs/owner.py
@@ -0,0 +1,57 @@
+import json
+import discord
+import settings
+import importlib
+from discord.ext import commands
+
+def guildcheck(ctx):
+    return ctx.guild.id == settings.ServerID
+
+class owner(commands.Cog):
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    @commands.check(guildcheck)
+    async def forcever(self, ctx: commands.Context, discordid: int, robloxid: int):
+        with open(settings.JSON_DIR) as f:
+            verified = json.load(f)
+        user_id_string = str(discordid)
+
+        if user_id_string in verified:
+            await ctx.send("User already verified")
+        else:
+            verified.update({discordid: robloxid})
+        
+            with open(settings.JSON_DIR, 'w') as json_file:
+                    json.dump(verified, json_file,
+                                        indent=4,
+                                        separators=(',',': '))
+            
+            await ctx.send("User verification added properly")
+
+    @commands.command()
+    @commands.is_owner()
+    @commands.check(guildcheck)
+    async def forceunver(self, ctx: commands.Context, discordid: int):
+        with open(settings.JSON_DIR) as f:
+            verified = json.load(f)
+        user_id_string = str(discordid)
+
+        if user_id_string not in verified:
+            await ctx.send("User not verified")
+        else:
+            verified.pop(user_id_string)
+
+            with open(settings.JSON_DIR, 'w') as json_file:
+                    json.dump(verified, json_file,
+                                        indent=4,
+                                        separators=(',',': '))
+                    
+            await ctx.send("User unverification removed properly")
+
+async def setup(bot):
+    importlib.reload(settings)
+    await bot.add_cog(owner(bot))
